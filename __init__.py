6944ca79327ee30f4cade1f375722aa82365346c
aborger <borger.aaron@gmail.com>
2024-12-15 15:34:55 -0500

    in progress

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 import os, sys
-code_dir = os.path.dirname(os.path.realpath(__file__))
-sys.path.append(code_dir)
+bundleSDF_dir = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(bundleSDF_dir)
 from bundlesdf import *
 from segmentation_utils import Segmenter
--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 ROOT=$(pwd)
 cd /kaolin && pip install -e .
 cd ${ROOT}/mycuda && rm -rf build *egg* && pip install -e .
-cd ${ROOT}/BundleTrack && rm -rf build && mkdir build && cd build && cmake .. && make -j11
+#cd ${ROOT}/BundleTrack && rm -rf build && mkdir build && cd build && cmake .. && make -j11
+cd ${ROOT}/BundleTrack/build && make -j11
